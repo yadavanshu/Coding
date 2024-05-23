26a40783ac13c6767b86e81ec0a970ca5c2364b5
Anshu Yadav <ayadav11@opentext.com>
2024-05-24 00:14:01 +0530

    second commit

---
+++ C++/vector.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> nums = {12,21,23,24,53,51,1,4,9,10,6,98,22};
+    sort(nums.begin(),nums.end());
+    for(auto xt:nums)
+        cout<<xt<<" ";
+    return 0;
+}
