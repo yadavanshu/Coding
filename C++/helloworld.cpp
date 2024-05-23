9c092b9df628e4ff86914cb97fd4a13eeca81c5a
Anshu Yadav <ayadav11@opentext.com>
2024-05-24 00:03:48 +0530

    first commit

---
+++ C++/helloworld.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Hello!!, World";
+    vector<int> nums = {12,12,32,14,53};
+    cout<<endl;
+    for(auto xt:nums)
+        cout<<xt<<" ";
+    return 0;
+}
