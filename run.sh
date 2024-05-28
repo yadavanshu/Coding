c40d185059746e1016bde0f280f4e6d1d52ba50f
Anshu Yadav <ayadav11@opentext.com>
2024-05-28 22:44:52 +0530

    Your commit message

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Your script starts here
+
+# Navigate to your Git repository directory
+
+# Perform Git commands
+
+
+git add .
+git commit -m "Your commit message"
+git push -u origin master
