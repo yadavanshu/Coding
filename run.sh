9262fd5e2560ff99a2cd7a9c6a4315194b50c120
Anshu Yadav <ayadav11@opentext.com>
2024-05-29 15:39:25 +0530

    Repo Update

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPO_LINK="https://github.com/yadavanshu/Coding.git"
+BRANCH="master"
+COMMIT_MSG="Repo Update"
+
+
+# git init
+git add .
+git commit -m "$COMMIT_MSG"
+git remote add origin "$REPO_LINK"
+git push -u origin "$BRANCH"
+
+echo "Git Update Done"
+
