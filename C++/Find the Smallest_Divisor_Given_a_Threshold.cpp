67299289d6e95652acdbd96c09074a84bae07277
Anshu Yadav <ayadav11@opentext.com>
2024-06-18 13:49:35 +0530

    Repo Update

--- C++/Find the Smallest_Divisor_Given_a_Threshold.cpp
+++ C++/Find the Smallest_Divisor_Given_a_Threshold.cpp
@@ -17,16 +17,26 @@ public:
     //     return ans;
     // }
     bool checkDivisor(vector<int>& nums, int threshold, int divisor){
-
+        int sum = 0;
+        int res = -1;
+        for(auto xt:nums){
+            sum += ceil(xt / (double)divisor);
+        }
+        return sum<=threshold;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int low = 1, high = *max_element(nums.begin(),nums.end());
+        int low = 1, high = *max_element(nums.begin(), nums.end());
         int ans = INT_MAX;
         while(low<=high){
-            int divisor = low + (high - low)/2;
-            if(checkDivisor(nums,threshold,divisor)){
+            int divisor = (low + high) / 2;
+            if (checkDivisor(nums, threshold, divisor))
+            {
                 ans = divisor;
-                
+                high = divisor - 1;
+            }
+            else
+            {
+                low = divisor + 1;
             }
         }
         return ans;
